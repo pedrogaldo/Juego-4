d304af0765d6c8f71aaed2db60cdfe02cc82ccf0
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2023-11-10 21:05:58 -0300

    inicio desarrollo
    empece UI y parte de los scripts

---
+++ Assets/Scripts/PreguntaYRespuesta.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+
+public class PreguntaYRespuesta
+{
+    public string Pregunta;
+    public string[] Respuestas;
+    public string RespuestaCorrecta;
+
+}
---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<PreguntaYRespuesta> QnA;
+    public GameObject[] opciones;
+    public int currentQuestion;
+
+    public Text textoPregunta;
+
+    private void Start()
+    {
+
+    }
+
+    void generaPreguntas()
+    {
+
+    }
+}
